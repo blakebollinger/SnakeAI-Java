ae9d633cb607dc3fa5fba1dd94052a355129bdb3
Blake Bollinger <blake.bollinger.2020@stpauls.com>
2020-03-27 11:44:28 -0500

    Added labels for the title and score

--- Display.java
+++ Display.java
@@ -7,7 +7,7 @@ public class Display extends JFrame {
     public Display() {
         super("Snake Graphics");
 
-        setSize(500, 500);
+        setSize(750, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
@@ -17,8 +17,20 @@ public class Display extends JFrame {
 
         Graphics2D g2d = (Graphics2D) g;
 
+        // Initializes text font
+        Font font = new Font("Helvetica", Font.PLAIN, 12);
+
+        g2d.setFont(font);
+
         // Border
-        g2d.drawRect(20, 40, 460, 440);
+        g2d.drawRect(20, 90, 710, 690);
+
+        // Title
+        String title = "Welcome to SnakeAI";
+        g2d.drawString(title, (750 - getFontMetrics(font).stringWidth(title)) / 2, 45);
+
+        // Score
+        g2d.drawString("Score: ", 20, 80);
 
     }
 
