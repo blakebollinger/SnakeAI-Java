1ef0837fd5361faec7b2b9cb926cf71abc76248d
Blake Bollinger <blake.bollinger.2020@stpauls.com>
2020-03-30 18:06:21 -0500

    Added more documentation

--- Snake.java
+++ Snake.java
@@ -11,13 +11,17 @@ public class Snake {
     }
 
     public static void initSnake(Graphics2D g2d) {
+        // Generate random starting coordinates
         Randomizer coords = new Randomizer();
-        System.out.println("x " + coords.getX());
-        System.out.println("y " + coords.getY());
+        System.out.println("Snake generated at X:" + coords.getX() + " Y:" + coords.getY());
 
+        // Draw snake at specified coordinates
         g2d.fillRect(coords.getX(), coords.getY(), 30, 30);
-        g2d.fillRect(coords.getX()+30, coords.getY(), 30, 30);
-        g2d.fillRect(coords.getX()+60, coords.getY(), 30, 30);
+        g2d.fillRect(coords.getX() + 30, coords.getY(), 30, 30);
+        g2d.fillRect(coords.getX() + 60, coords.getY(), 30, 30);
+
+        // Display completion message
+        System.out.println("Snake generated at X:" + coords.getX() + " Y:" + coords.getY());
 
     }
 
--- SnakeGame.java
+++ SnakeGame.java
@@ -4,7 +4,10 @@ public class SnakeGame {
 
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to Snake");
+
+        System.out.println("Generating game...");
 
         // Quick Swing implementation drawing our game
         SwingUtilities.invokeLater(() -> new Display().setVisible(true));
