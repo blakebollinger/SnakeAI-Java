469d2209d228f98998b583ec350fc81f236338dd
Blake <bbmagic33@gmail.com>
2020-03-30 21:30:26 -0500

    SNAKE NOW SPAWNS WITH RANDOM DIRECTION

--- Randomizer.java
+++ Randomizer.java
@@ -2,17 +2,38 @@ public class Randomizer {
 
     private int y;
     private int x;
+    private int direction;
 
-    public Randomizer(){
-        y = (int)(Math.random() * (23 - 6));
-        x = (int)(Math.random() * (23 - 6));
+    public Randomizer() {
+
+        y = ((int) (Math.random() * (23 - 6))) + 3;
+        x = ((int) (Math.random() * (23 - 6))) + 3;
+        direction = (int) (Math.random() * 4);
+        System.out.println(direction);
     }
 
-    public int getY(){
+    public String getDirection() {
+
+        switch (direction) {
+            case 0:
+                return "up";
+            case 1:
+                return "right";
+            case 2:
+                return "down";
+            case 3:
+                return "left";
+            default:
+                System.out.println("Direction didn't match 0-3");
+                return null;
+        }
+    }
+
+    public int getY() {
         return y;
     }
 
-    public int getX(){
+    public int getX() {
         return x;
     }
 }
--- Snake.java
+++ Snake.java
@@ -15,15 +15,39 @@ public class Snake {
     public static void initSnake(Graphics2D g2d) {
         // Generate random starting coordinates
         Randomizer startingCoords = new Randomizer();
-        getPosition().add(new Coordinate(startingCoords.getX(), startingCoords.getY()));
-        getPosition().add(new Coordinate(startingCoords.getX() + 1, startingCoords.getY()));
-        getPosition().add(new Coordinate(startingCoords.getX() + 2, startingCoords.getY()));
+        initDirection(startingCoords);
 
         // Draw snake at specified coordinates
         Display.drawSnake(g2d);
 
         // Display completion message
         System.out.println("Snake generated at X:" + startingCoords.getX() + " Y:" + startingCoords.getY());
+
+    }
+
+    public static void initDirection(Randomizer r) {
+        switch (r.getDirection()) {
+            case "up":
+                getPosition().add(new Coordinate(r.getX(), r.getY()));
+                getPosition().add(new Coordinate(r.getX(), r.getY() - 1));
+                getPosition().add(new Coordinate(r.getX(), r.getY() - 2));
+                break;
+            case "right":
+                getPosition().add(new Coordinate(r.getX(), r.getY()));
+                getPosition().add(new Coordinate(r.getX() - 1, r.getY()));
+                getPosition().add(new Coordinate(r.getX() - 2, r.getY()));
+                break;
+            case "down":
+                getPosition().add(new Coordinate(r.getX(), r.getY()));
+                getPosition().add(new Coordinate(r.getX(), r.getY() + 1));
+                getPosition().add(new Coordinate(r.getX(), r.getY() + 2));
+                break;
+            case "left":
+                getPosition().add(new Coordinate(r.getX(), r.getY()));
+                getPosition().add(new Coordinate(r.getX() + 1, r.getY()));
+                getPosition().add(new Coordinate(r.getX() + 2, r.getY()));
+                break;
+        }
 
     }
 
